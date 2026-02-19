8e65ad03367207e1a7fc3ac4839beae50b6cae93
hari <saisrihari0403@gmail.com>
2026-02-19 16:30:16 +0530

    V1

---
+++ irx_mmm_sourcecode/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <h1>🚀 THIS IS -ITROOTIX </h1>
+      <p>Built with Vite + React</p>
+      
+    </div>
+  );
+}
+
+export default App;
